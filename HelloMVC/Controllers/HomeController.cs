1b42385207bb74143126b860722969fc61994964
herkommer <m@herkommer.co.uk>
2020-01-11 14:37:50 +0100

    initial commit

---
+++ HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using HelloMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hello from MVC";
+        }
+        public string yo()
+        {
+            return "hey hey!";
+        }
+
+        public ViewResult about()
+        {
+            return View();
+        }
+        public ViewResult contact()
+        {
+            string data = "ACME Corporation 2020";
+            return View(data as object);
+        }
+        public ViewResult productinfo()
+        {
+            Product p = new Product();
+            p.Name = "Kockkniv";
+            p.Price = 100;
+
+            return View(p);
+        }
+        public ViewResult productlist()
+        {
+            Product p = new Product();
+            p.Name = "Kockkniv";
+            p.Price = 100;
+
+            List<Product> list = new List<Product>();
+            list.Add(p);
+            list.Add(p);
+            list.Add(p);
+            list.Add(new Product() { Price = 300, Name = "Förkläde" });
+
+            return View(list);
+        }
+    }
+}
